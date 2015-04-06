a1f096a7435cceb9f993cc5ab9f3a7ba433339c0
Tyler Audette <tyleraudette@yahoo.com>
2015-04-06 10:54:51 -0600

    done

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert('Hello Boulder!');
+// alert('Hello Boulder!');
+
+var person = prompt("What is your name?");
+
+if (person.length > 0) {
+	alert("Hello " + person +"!");
+}
+
+else {
+	alert("Please enter your name")
+}
